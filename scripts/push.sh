810b4f993c20e0e67301a132ae4e0d567ed71786
Adam J Lemmon <adamjlemmon@gmail.com>
2018-06-07 15:01:50 -0400

    add docker to travis ci

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,6 +5,6 @@ echo "Push in progress..."
 
 docker login --username ${DOCKER_USER} --password ${DOCKER_PASS}
 
-docker push blockchainlg/kc-controller:${BUILD_TAG}
-docker push blockchainlg/kc-db-service:${BUILD_TAG}
-docker push blockchainlg/kc-eth-service:${BUILD_TAG}
+docker push blockchainlg/kc-controller${BUILD_TAG}
+docker push blockchainlg/kc-db-service${BUILD_TAG}
+docker push blockchainlg/kc-eth-service${BUILD_TAG}
